3d21465ec6a00748cc55214d28a940a08ab6d603
anasmuhammadhamdan <anasmuhammadhamdan@gmail.com>
2024-03-03 15:33:33 +0500

    practice

---
+++ ismail.dart
@@ -0,0 +1,108 @@
+// void main() {
+
+// var english = 10;
+// var urdu = 10;
+// var math = 15;
+// var totalMarks  = 300;
+// var myTotalMarks = english+urdu+math;
+// print("Total Marks obtain by me  $myTotalMarks");
+// var percentage  =  myTotalMarks*100/300;
+// print("Percentage $percentage");
+
+// if(percentage>80){
+//   print("MY Grade is A+");
+// }else if (percentage>70){
+//     print("MY Grade is A");
+
+// }else if(percentage>60){
+//     print("MY Grade is b");
+
+// }else if(percentage>55){
+//     print("MY Grade is c");
+
+// }
+// else if(percentage>45){
+//     print("MY Grade is d");
+
+// }else if(percentage>33){
+//     print("MY Grade is e ");
+
+// }else{
+//   print ("YOURE FAILED");
+// }
+
+ 
+// }
+
+
+// ==
+// >
+// <
+// %
+// != 
+
+// ? 
+// : 
+
+
+// void main() {
+
+
+//  print("Celcius to Farhenite ");
+ 
+//  double celciusTemperature = 22.6;
+ 
+//  double  fahrenihetTemperature = (celciusTemperature*9/5)+32;
+//   print("Fahrenite Temperature is ${fahrenihetTemperature} ");
+
+//  print("Fahreniyte to Celcius ");
+
+//   double  celcius = (fahrenihetTemperature-32)*5/9;
+
+//   print("Celcius Temperature is ${celcius} ");
+  
+
+ 
+// 
+
+
+void main (){
+//   int side1=12;
+//   int side2=22;
+//   int length=11;
+//   int breath=33;
+//    int squarearea=(side1*side2);
+//   print(squarearea);
+//   int recarea=(length*breath);
+//   print(recarea);
+  
+//   question no 3
+  
+//   double tempcels=23;
+//   double tempfern=22;
+//   double resultem=(tempcels*9/5)+32;
+//   print(resultem);
+//   double resultem2=(tempfern-32)*5/9;
+//   print(resultem2);
+
+  
+//   question no 4 
+  
+  var  english =12;
+  var urdu =0;
+  var math = 43;
+   var totalmarks =(english+urdu+math);
+  print(totalmarks);
+  
+   if(totalmarks>80){
+     print("A+");
+   }else if(totalmarks>70){
+     print("A");
+     
+   }else if (totalmarks>60){
+     print("b");
+   }else{
+     print("failed");
+   }
+  
+}
